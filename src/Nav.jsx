9051df3fc58e7d1fba543daa7a40344a0f5a3531
Chazz Carrizales <czcarrizales@yahoo.com>
2025-10-14 13:21:54 -0700

    initial styling of Nav and added Google Font

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,20 +2,25 @@ import './Nav.css'
 
 function Nav() {
 
-  return (
-    <>
-      <div>
-        <a href="">Schedule</a>
-        <a href="">Classes</a>
-        <a href="">Students</a>
-        <select name="" id="">
-            <option value="user">user</option>
-            <option value="user2">user2</option>
-        </select>
-        <button>Add Student</button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className='nav-main'>
+                <div className='nav-links'>
+                    <a href="" className='nav-link'>Schedule</a>
+                    <a href="" className='nav-link'>Classes</a>
+                    <a href="" className='nav-link'>Students</a>
+                </div>
+                <div className='nav-offset'>
+                    <select name="" id="" className='student-dropdown'>
+                        <option value="user">user</option>
+                        <option value="user2">user2</option>
+                    </select>
+                    <button className='add-student'>Add Student</button>
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default Nav
