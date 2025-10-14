5c724dd0ea2db1fce4a05fdde6951d22b2a8f4cf
Chazz Carrizales <czcarrizales@yahoo.com>
2025-10-14 12:00:45 -0700

    starter code (Nav and ScheduleView)

---
+++ src/Nav.jsx
@@ -0,0 +1,21 @@
+import './Nav.css'
+
+function Nav() {
+
+  return (
+    <>
+      <div>
+        <a href="">Schedule</a>
+        <a href="">Classes</a>
+        <a href="">Students</a>
+        <select name="" id="">
+            <option value="user">user</option>
+            <option value="user2">user2</option>
+        </select>
+        <button>Add Student</button>
+      </div>
+    </>
+  )
+}
+
+export default Nav
