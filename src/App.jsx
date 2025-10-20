72ccf9c740a2aa68c4afc5511d6b36bef7426ed3
Chazz Carrizales <czcarrizales@yahoo.com>
2025-10-20 15:01:32 -0700

    main state for mainStudent and allStudents

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,39 @@ import ClassesView from './ClassesView'
 import AddStudentView from './AddStudentView'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import StudentRow from './StudentRow'
+import Class from './models/Class.js'
+import Student from './models/Student.js'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [mainStudent, setMainStudent] = useState({})
+  const [allStudents, setAllStudents] = useState([])
+
+  useEffect(() => {
+    Class.seed()
+    console.log(Class.list())
+    setAllStudents(Student.list())
+  }, [])
+
+  useEffect(() => {
+    console.log('this is the main student')
+    console.log(mainStudent)
+  }, [mainStudent])
+
+  useEffect(() => {
+    console.log('students list has changed!')
+  }, [allStudents])
 
   return (
     <>
       <BrowserRouter>
-        <Nav />
+        <Nav mainStudent={mainStudent} setMainStudent={setMainStudent} allStudents={allStudents} />
         <Routes>
-          <Route path='/' element={<ScheduleView/>} />
-          <Route path='/students' element={<StudentsView/>} />
-          <Route path='/classes' element={<ClassesView/>} />
-          <Route path='/addstudent' element={<AddStudentView/>} />
+          <Route path='/' element={<ScheduleView mainStudent={mainStudent}/>} />
+          <Route path='/students' element={<StudentsView allStudents={allStudents} setAllStudents={setAllStudents}/>} />
+          <Route path='/classes' element={<ClassesView mainStudent={mainStudent} setMainStudent={setMainStudent}/>} />
+          <Route path='/addstudent' element={<AddStudentView setAllStudents={setAllStudents}/>} />
         </Routes>
       </BrowserRouter>
 
